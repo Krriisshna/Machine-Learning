dd094940885496bf1c53bd05451287f8888fc3eb
Krishna <kriisshhna.0@gmail.com>
2026-09-18 12:28:07 +0530

    function fix

--- Coursera/Supervised ML (Regression and Classification)/03_Classification/function.py
+++ Coursera/Supervised ML (Regression and Classification)/03_Classification/function.py
@@ -1,3 +1,4 @@
+import time
 import matplotlib.pyplot as plt
 from matplotlib.patches import FancyArrowPatch
 import numpy as np
@@ -87,7 +88,7 @@ def sigmoid(z):
     g = (1.0)/(1.0 + np.exp(-z))
     return g
 
-def plt_logistic_cost(X,y):
+def tlogistic_cost(X,y):
     """ plots logistic cost """
     wx, by = np.meshgrid(np.linspace(-6,12,50),
                          np.linspace(0, -20, 40))
@@ -297,51 +298,138 @@ def plt_tumor_data(x,y,ax):
     ax.set_xlabel('Tumor Size')
     ax.set_title("Logistic Regression on Categorical Data")
 
+def gradient_descent(X, y, w_in, b_in, alpha, num_iters,
+                      logistic=False, lambda_=0, verbose=True):
+    """Batch gradient descent used by the interactive logistic plot."""
+    X = np.asarray(X)
+    y = np.asarray(y).reshape(-1, 1)
+    w = np.asarray(w_in, dtype=float).reshape(-1, 1).copy()
+    b = float(b_in)
+    m = X.shape[0]
+    J_history = []
+
+    for i in range(num_iters):
+        z = X @ w + b
+
+        if logistic:
+            f = sigmoid(z)
+        else:
+            f = z
+
+        dj_dw = (X.T @ (f - y)) / m
+        dj_db = np.sum(f - y) / m
+
+        if lambda_ != 0:
+            dj_dw += (lambda_ / m) * w
+
+        w -= alpha * dj_dw
+        b -= alpha * dj_db
+
+        if i < 100000:
+            J_history.append(
+                compute_cost_matrix(
+                    X, y, w, b,
+                    logistic=logistic,
+                    lambda_=lambda_,
+                    safe=True
+                )
+            )
+
+        if verbose and (i % max(1, num_iters // 10) == 0):
+            print(f"Iteration {i:4d}: Cost {J_history[-1]:8.4f}")
+
+    return w, b, J_history
+
+
 class plt_quad_logistic:
     ''' plots a quad plot showing logistic regression '''
     # pylint: disable=too-many-instance-attributes
     # pylint: disable=too-many-locals
     # pylint: disable=missing-function-docstring
     # pylint: disable=attribute-defined-outside-init
-    def __init__(self, x_train,y_train, w_range, b_range):
-        # setup figure
-        fig = plt.figure( figsize=(10,6))
+
+    def __init__(self, x_train, y_train, w_range, b_range):
+        fig = plt.figure(figsize=(10, 6))
         fig.canvas.toolbar_visible = False
         fig.canvas.header_visible = False
         fig.canvas.footer_visible = False
-        fig.set_facecolor('#ffffff') #white
-        gs  = GridSpec(2, 2, figure=fig)
+        fig.set_facecolor('#ffffff')
+
+        gs = GridSpec(2, 2, figure=fig)
         ax0 = fig.add_subplot(gs[0, 0])
         ax1 = fig.add_subplot(gs[0, 1])
-        ax2 = fig.add_subplot(gs[1, 0],  projection='3d')
-        ax3 = fig.add_subplot(gs[1,1])
-        pos = ax3.get_position().get_points()  ##[[lb_x,lb_y], [rt_x, rt_y]]
-        h = 0.05 
+        ax2 = fig.add_subplot(gs[1, 0], projection='3d')
+        ax3 = fig.add_subplot(gs[1, 1])
+
+        pos = ax3.get_position().get_points()
+        h = 0.05
         width = 0.2
-        axcalc   = plt.axes([pos[1,0]-width, pos[1,1]-h, width, h])  #lx,by,w,h
+        axcalc = plt.axes([pos[1, 0] - width, pos[1, 1] - h, width, h])
         ax = np.array([ax0, ax1, ax2, ax3, axcalc])
+
         self.fig = fig
         self.ax = ax
         self.x_train = x_train
         self.y_train = y_train
 
-        self.w = 0. #initial point, non-array
+        self.w = 0.
         self.b = 0.
 
-        # initialize subplots
         self.dplot = data_plot(ax[0], x_train, y_train, self.w, self.b)
-        self.con_plot = contour_and_surface_plot(ax[1], ax[2], x_train, y_train, w_range, b_range, self.w, self.b)
+        self.con_plot = contour_and_surface_plot(
+            ax[1], ax[2], x_train, y_train, w_range, b_range, self.w, self.b
+        )
         self.cplot = cost_plot(ax[3])
 
-        # setup events
         self.cid = fig.canvas.mpl_connect('button_press_event', self.click_contour)
-        self.bcalc = Button(axcalc, 'Run Gradient Descent \nfrom current w,b (click)', color=dlc["dlorange"])
+        self.bcalc = Button(
+            axcalc,
+            'Run Gradient Descent \\nfrom current w,b (click)',
+            color=dlc["dlorange"]
+        )
         self.bcalc.on_clicked(self.calc_logistic)
+
+    def click_contour(self, event):
+        if event.inaxes == self.ax[1]:
+            self.w = event.xdata
+            self.b = event.ydata
+
+            self.cplot.re_init()
+            self.dplot.update(self.w, self.b)
+            self.con_plot.update_contour_wb_lines(self.w, self.b)
+            self.con_plot.path.re_init(self.w, self.b)
+
+            self.fig.canvas.draw()
+
+    def calc_logistic(self, event):
+        for it in [1, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096]:
+            w, self.b, J_hist = gradient_descent(
+                self.x_train.reshape(-1, 1),
+                self.y_train.reshape(-1, 1),
+                np.array(self.w).reshape(-1, 1),
+                self.b,
+                0.1,
+                it,
+                logistic=True,
+                lambda_=0,
+                verbose=False
+            )
+
+            self.w = w[0, 0]
+            self.dplot.update(self.w, self.b)
+            self.con_plot.update_contour_wb_lines(self.w, self.b)
+            self.con_plot.path.add_path_item(self.w, self.b)
+            self.cplot.add_cost(J_hist)
+
+            time.sleep(0.3)
+            self.fig.canvas.draw()
+
 
 class data_plot:
     ''' handles data plot '''
     # pylint: disable=missing-function-docstring
     # pylint: disable=attribute-defined-outside-init
+
     def __init__(self, ax, x_train, y_train, w, b):
         self.ax = ax
         self.x_train = x_train
@@ -356,44 +444,165 @@ class data_plot:
 
         self.ax.autoscale(enable=False)
 
+    def plt_tumor_data(self):
+        x = self.x_train
+        y = self.y_train
+        pos = y == 1
+        neg = y == 0
+
+        self.ax.scatter(
+            x[pos], y[pos], marker='x', s=80, c='red', label="malignant"
+        )
+        self.ax.scatter(
+            x[neg], y[neg], marker='o', s=100, label="benign",
+            facecolors='none', edgecolors=dlc["dlblue"], lw=3
+        )
+        self.ax.set_ylim(-0.175, 1.1)
+        self.ax.set_ylabel('y')
+        self.ax.set_xlabel('Tumor Size')
+        self.ax.set_title("Logistic Regression on Categorical Data")
+
+    def update(self, w, b):
+        self.w = w
+        self.b = b
+        self.draw_logistic_lines()
+        self.mk_cost_lines()
+
+    def draw_logistic_lines(self, firsttime=False):
+        if not firsttime:
+            self.aline[0].remove()
+            self.bline[0].remove()
+            self.alegend.remove()
+
+        xlim = self.ax.get_xlim()
+        x_hat = np.linspace(*xlim, 30)
+
+        y_hat = sigmoid(np.dot(x_hat.reshape(-1, 1), self.w) + self.b)
+        self.aline = self.ax.plot(
+            x_hat, y_hat, color=dlc["dlblue"], label="y = sigmoid(z)"
+        )
+
+        f_wb = np.dot(x_hat.reshape(-1, 1), self.w) + self.b
+        self.bline = self.ax.plot(
+            x_hat, f_wb, color=dlc["dlorange"], lw=1,
+            label=f"z = {np.squeeze(self.w):0.2f}x+({self.b:0.2f})"
+        )
+        self.alegend = self.ax.legend(loc='upper left')
+
+    def mk_cost_lines(self, firsttime=False):
+        if not firsttime:
+            for artist in self.cost_items:
+                artist.remove()
+
+        self.cost_items = []
+        cstr = f"cost = (1/{self.m})*("
+        ctot = 0
+        label = 'cost for point'
+        addedbreak = False
+
+        for p in zip(self.x_train, self.y_train):
+            f_wb_p = sigmoid(self.w * p[0] + self.b)
+            c_p = compute_cost_matrix(
+                p[0].reshape(-1, 1),
+                p[1],
+                np.array(self.w),
+                self.b,
+                logistic=True,
+                lambda_=0,
+                safe=True
+            )
+
+            a = self.ax.vlines(
+                p[0], p[1], f_wb_p, lw=3,
+                color=dlc["dlpurple"], ls='dotted', label=label
+            )
+            label = ''
+
+            cxy = [p[0], p[1] + (f_wb_p - p[1]) / 2]
+            b = self.ax.annotate(
+                f'{c_p:0.1f}',
+                xy=cxy,
+                xycoords='data',
+                color=dlc["dlpurple"],
+                xytext=(5, 0),
+                textcoords='offset points'
+            )
+
+            cstr += f"{c_p:0.1f} +"
+            if len(cstr) > 38 and not addedbreak:
+                cstr += "\\n"
+                addedbreak = True
+
+            ctot += c_p
+            self.cost_items.extend((a, b))
+
+        ctot = ctot / len(self.x_train)
+        cstr = cstr[:-1] + f") = {ctot:0.2f}"
+
+        c = self.ax.text(
+            0.05, 0.02, cstr,
+            transform=self.ax.transAxes,
+            color=dlc["dlpurple"]
+        )
+        self.cost_items.append(c)
+
+
 class contour_and_surface_plot:
     ''' plots combined in class as they have similar operations '''
     # pylint: disable=missing-function-docstring
     # pylint: disable=attribute-defined-outside-init
-    def __init__(self, axc, axs, x_train, y_train, w_range, b_range, w, b):
 
+    def __init__(self, axc, axs, x_train, y_train, w_range, b_range, w, b):
         self.x_train = x_train
         self.y_train = y_train
         self.axc = axc
         self.axs = axs
 
-        #setup useful ranges and common linspaces
-        b_space  = np.linspace(*b_range, 100)
-        w_space  = np.linspace(*w_range, 100)
+        b_space = np.linspace(*b_range, 100)
+        w_space = np.linspace(*w_range, 100)
 
-        # get cost for w,b ranges for contour and 3D
-        tmp_b,tmp_w = np.meshgrid(b_space,w_space)
+        tmp_b, tmp_w = np.meshgrid(b_space, w_space)
         z = np.zeros_like(tmp_b)
+
         for i in range(tmp_w.shape[0]):
             for j in range(tmp_w.shape[1]):
-                z[i,j] = compute_cost_matrix(x_train.reshape(-1,1), y_train, tmp_w[i,j], tmp_b[i,j],
-                                             logistic=True, lambda_=0, safe=True)
-                if z[i,j] == 0:
-                    z[i,j] = 1e-9
+                z[i, j] = compute_cost_matrix(
+                    x_train.reshape(-1, 1),
+                    y_train,
+                    tmp_w[i, j],
+                    tmp_b[i, j],
+                    logistic=True,
+                    lambda_=0,
+                    safe=True
+                )
+                if z[i, j] == 0:
+                    z[i, j] = 1e-9
 
-        ### plot contour ###
-        CS = axc.contour(tmp_w, tmp_b, np.log(z),levels=12, linewidths=2, alpha=0.7,colors=dlcolors)
+        axc.contour(
+            tmp_w, tmp_b, np.log(z),
+            levels=12, linewidths=2, alpha=0.7, colors=dlcolors
+        )
         axc.set_title('log(Cost(w,b))')
         axc.set_xlabel('w', fontsize=10)
         axc.set_ylabel('b', fontsize=10)
         axc.set_xlim(w_range)
         axc.set_ylim(b_range)
-        self.update_contour_wb_lines(w, b, firsttime=True)
-        axc.text(0.7,0.05,"Click to choose w,b",  bbox=dict(facecolor='white', ec = 'black'), fontsize = 10,
-                transform=axc.transAxes, verticalalignment = 'center', horizontalalignment= 'center')
 
-        #Surface plot of the cost function J(w,b)
-        axs.plot_surface(tmp_w, tmp_b, z,  cmap = cm.jet, alpha=0.3, antialiased=True)
+        self.update_contour_wb_lines(w, b, firsttime=True)
+
+        axc.text(
+            0.7, 0.05, "Click to choose w,b",
+            bbox=dict(facecolor='white', ec='black'),
+            fontsize=10,
+            transform=axc.transAxes,
+            verticalalignment='center',
+            horizontalalignment='center'
+        )
+
+        axs.plot_surface(
+            tmp_w, tmp_b, z, cmap=cm.jet,
+            alpha=0.3, antialiased=True
+        )
         axs.plot_wireframe(tmp_w, tmp_b, z, color='k', alpha=0.1)
         axs.set_xlabel("$w$")
         axs.set_ylabel("$b$")
@@ -407,7 +616,83 @@ class contour_and_surface_plot:
         axs.autoscale(enable=False)
         axc.autoscale(enable=False)
 
-        self.path = path(self.w,self.b, self.axc)
+        self.path = path(self.w, self.b, self.axc)
+
+    def update_contour_wb_lines(self, w, b, firsttime=False):
+        self.w = w
+        self.b = b
+
+        cst = compute_cost_matrix(
+            self.x_train.reshape(-1, 1),
+            self.y_train,
+            np.array(self.w),
+            self.b,
+            logistic=True,
+            lambda_=0,
+            safe=True
+        )
+
+        if not firsttime:
+            for artist in self.dyn_items:
+                artist.remove()
+
+        a = self.axc.scatter(
+            self.w, self.b, s=100, color=dlc["dlblue"],
+            zorder=10, label="cost with \ncurrent w,b"
+        )
+        b_line = self.axc.hlines(
+            self.b, self.axc.get_xlim()[0], self.w,
+            lw=4, color=dlc["dlpurple"], ls='dotted'
+        )
+        c = self.axc.vlines(
+            self.w, self.axc.get_ylim()[0], self.b,
+            lw=4, color=dlc["dlpurple"], ls='dotted'
+        )
+        d = self.axc.annotate(
+            f"Cost: {cst:0.2f}",
+            xy=(self.w, self.b),
+            xytext=(4, 4),
+            textcoords='offset points',
+            bbox=dict(facecolor='white'),
+            size=10
+        )
+        e = self.axs.scatter3D(
+            self.w, self.b, cst, marker='X', s=100
+        )
+
+        self.dyn_items = [a, b_line, c, d, e]
+
+
+class path:
+    ''' tracks paths during gradient descent on contour plot '''
+    # pylint: disable=missing-function-docstring
+    # pylint: disable=attribute-defined-outside-init
+
+    def __init__(self, w, b, ax):
+        self.path_items = []
+        self.w = w
+        self.b = b
+        self.ax = ax
+
+    def re_init(self, w, b):
+        for artist in self.path_items:
+            artist.remove()
+        self.path_items = []
+        self.w = w
+        self.b = b
+
+    def add_path_item(self, w, b):
+        a = FancyArrowPatch(
+            posA=(self.w, self.b),
+            posB=(w, b),
+            color=dlc["dlblue"],
+            arrowstyle='simple, head_width=5, head_length=10, tail_width=0.0',
+        )
+        self.ax.add_artist(a)
+        self.path_items.append(a)
+        self.w = w
+        self.b = b
+
 
 class cost_plot:
     """ manages cost plot for plt_quad_logistic """
